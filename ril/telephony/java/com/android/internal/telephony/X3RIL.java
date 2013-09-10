5b0de45a6f4c8dd205b0e08fcdec6933e40291f9
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-09-10 22:50:06 +0200

    initial upload

---
+++ ril/telephony/java/com/android/internal/telephony/X3RIL.java
@@ -0,0 +1,197 @@
+package com.android.internal.telephony;
+
+import static com.android.internal.telephony.RILConstants.*;
+
+import android.content.Context;
+import android.os.AsyncResult;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcel;
+
+import android.telephony.PhoneNumberUtils;
+
+public class X3RIL extends RIL implements CommandsInterface {
+    private boolean sentHwBootstrap = false;
+
+    public X3RIL(Context context, int networkMode, int cdmaSubscription) {
+        super(context, networkMode, cdmaSubscription);
+    }
+
+    @Override
+    public void
+    getIMEI(Message result) {
+        //RIL_REQUEST_LGE_SEND_COMMAND 0
+        RILRequest rrLSC = RILRequest.obtain(
+                0x113, null);
+        rrLSC.mParcel.writeInt(1);
+        rrLSC.mParcel.writeInt(0);
+        send(rrLSC);
+
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_GET_IMEI, result);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
+        send(rr);
+    }
+
+    @Override
+    public void
+    queryCallForwardStatus(int cfReason, int serviceClass,
+                String number, Message response) {
+        RILRequest rr
+            = RILRequest.obtain(RIL_REQUEST_QUERY_CALL_FORWARD_STATUS, response);
+
+        rr.mParcel.writeInt(2); // 2 is for query action, not in use anyway
+        rr.mParcel.writeInt(cfReason);
+        if (serviceClass == 0)
+            serviceClass = 255;
+        rr.mParcel.writeInt(serviceClass);
+        rr.mParcel.writeInt(PhoneNumberUtils.toaFromString(number));
+        rr.mParcel.writeString(number);
+        rr.mParcel.writeInt (0);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+                + " " + cfReason + " " + serviceClass);
+
+        send(rr);
+    }
+
+    static final int RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE = 1050;
+    static final int RIL_UNSOL_LGE_XCALLSTAT = 1053;
+    static final int RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC = 1074;
+    static final int RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW = 1061;
+
+    @Override
+    protected void
+    processUnsolicited (Parcel p) {
+        Object ret;
+        int dataPosition = p.dataPosition(); // save off position within the Parcel
+        int response = p.readInt();
+
+        switch(response) {
+            case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_XCALLSTAT: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW: ret =  responseVoid(p); break;
+            default:
+                // Rewind the Parcel
+                p.setDataPosition(dataPosition);
+
+                // Forward responses that we are not overriding to the super class
+                super.processUnsolicited(p);
+                return;
+        }
+        switch(response) {
+            case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED:
+                /* has bonus radio state int */
+                RadioState newState = getRadioStateFromInt(p.readInt());
+                p.setDataPosition(dataPosition);
+                super.processUnsolicited(p);
+                if (RadioState.RADIO_ON == newState) {
+                    setNetworkSelectionModeAutomatic(null);
+                }
+                return;
+            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE:
+            case RIL_UNSOL_LGE_XCALLSTAT:
+            case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC:
+                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
+                break;
+            case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW:
+                if (RILJ_LOGD) unsljLog(response);
+
+                if (mIccStatusChangedRegistrants != null) {
+                    mIccStatusChangedRegistrants.notifyRegistrants();
+                }
+                break;
+        }
+    }
+
+    protected void
+    processSolicited (Parcel p) {
+        int serial, error;
+        boolean found = false;
+        int dataPosition = p.dataPosition(); // save off position within the Parcel
+        serial = p.readInt();
+        error = p.readInt();
+
+        RILRequest rr = null;
+
+        /* Pre-process the reply before popping it */
+        synchronized (mRequestList) {
+            for (int i = 0, s = mRequestList.size() ; i < s ; i++) {
+                RILRequest tr = mRequestList.get(i);
+                if (tr.mSerial == serial) {
+                    if (error == 0 || p.dataAvail() > 0) {
+                        try {switch (tr.mRequest) {
+                            /* Get those we're interested in */
+                            case 0x113:
+                                rr = tr;
+                                break;
+                        }} catch (Throwable thr) {
+                            // Exceptions here usually mean invalid RIL responses
+                            if (rr.mResult != null) {
+                                AsyncResult.forMessage(rr.mResult, null, thr);
+                                rr.mResult.sendToTarget();
+                            }
+                            rr.release();
+                            return;
+                        }
+                    } 
+                }
+            }
+        }
+
+        if (rr == null) {
+            /* Nothing we care about, go up */
+            p.setDataPosition(dataPosition);
+
+            // Forward responses that we are not overriding to the super class
+            super.processSolicited(p);
+        }
+
+
+        rr = findAndRemoveRequestFromList(serial);
+
+        if (rr == null) {
+            return;
+        }
+
+        Object ret = null;
+
+        if (error == 0 || p.dataAvail() > 0) {
+            switch (rr.mRequest) {
+                case 0x113: ret =  responseVoid(p); break;
+                default:
+                    throw new RuntimeException("Unrecognized solicited response: " + rr.mRequest);
+            }
+            //break;
+        }
+
+        switch (rr.mRequest) {
+            case 0x113:
+                riljLog(rr.serialString() + "< LGE_SEND_COMMAND"
+                        + " " + retToString(rr.mRequest, ret));
+                /* COMMAND 1 inits hardware-related properties. We
+                 * only need it once per power cycle */
+                if (!sentHwBootstrap) {
+                    RILRequest rrLSC = RILRequest.obtain(
+                            0x113, null);
+                    rrLSC.mParcel.writeInt(1);
+                    rrLSC.mParcel.writeInt(1);
+                    send(rrLSC);
+                    sentHwBootstrap = true;
+                }
+                break;
+        }
+
+
+        if (rr.mResult != null) {
+            AsyncResult.forMessage(rr.mResult, ret, null);
+            rr.mResult.sendToTarget();
+        }
+
+        rr.release();
+    }
+
+}
